45b66489aac4ebe712504144e903a98ac606374c
HaniehGharaee <gharaie.hanieh@gmail.com>
2024-12-15 16:30:54 +0330

    Mongodb connection and create .env

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const dbConnect = async() =>{
     try{
-        await mongoose.connect('url')//Have to correct url
+        console.log(process.env)
+        await mongoose.connect(process.env.MONGO_URL)//Have to correct url
         console.log('DB Connection Successfully')
-
     }catch (error){
         console.log('DB Connection failed', error.message)
     }
