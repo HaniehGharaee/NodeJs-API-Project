b8887d66e84c768cdcefaf8fd2c95a89bca6454c
HaniehGharaee <gharaie.hanieh@gmail.com>
2025-01-07 08:50:36 +0330

    Custom Middleware

--- app/app.js
+++ app/app.js
@@ -6,12 +6,45 @@ const app = express();
 
 //============Middleware===========
 app.use(morgan("dev"));
-app.use(express.json())//pass incoming json data
+app.use(express.json()); //pass incoming json data
 
+app.use((req, res, next) => {
+  console.log("Middleware", req);
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+let user = {
+  name: "Hanieh Gharaee",
+  isAdmin: false,
+  isLogin: true, //test with postman to false
+};
+
+const isLogin = (req, res, next) => {
+  if (user.isLogin) {
+    next();
+  } else {
+    res.status(401).json({
+      msg: "Unauthorized",
+    });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (user.isAdmin) {
+    next();
+  } else {
+    res.status(401).json({
+      msg: "Unauthorized, you are not admin",
+    });
+  }
+};
+
+app.use(isLogin, isAdmin);
 //Routes
-
 //admin register
 app.use("/api/v1/admins", adminRouter);
+
 // app.use('/me', (req, res)=> {
 //     res.json({
 //         msg: "I will be called"
